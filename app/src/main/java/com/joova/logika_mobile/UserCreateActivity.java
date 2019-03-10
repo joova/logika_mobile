e055f2a464ce1ed72df32158b6bdac4f0154d0f8
Nur Hasyim <nur.hasyim@gmail.com>
2019-03-10 10:41:51 +0700

    Add user list & user create activity

---
+++ app/src/main/java/com/joova/logika_mobile/UserCreateActivity.java
@@ -0,0 +1,13 @@
+package com.joova.logika_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_create);
+    }
+}
