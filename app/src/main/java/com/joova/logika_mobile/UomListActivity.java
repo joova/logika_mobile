cd66a81620869b0eebe12ec6238cf25f2bdbff8e
Nur Hasyim <nur.hasyim@gmail.com>
2019-03-10 13:47:32 +0700

    Update list uom dengan menggunakan data dummy

--- app/src/main/java/com/joova/logika_mobile/UomListActivity.java
+++ app/src/main/java/com/joova/logika_mobile/UomListActivity.java
@@ -6,8 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class UomListActivity extends AppCompatActivity {
+
+    final String[] uomArray = {
+      "Mili Meter", "Centi Meter", "Desi Meter", "Deka Meter", "Kilo Meter", "Meter",
+            "Gram", "Ons", "Kilo Gram", "Liter", "Mili Liter"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,12 @@ public class UomListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_uom_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //buat list adapter
+        ArrayAdapter<String> uomAdpter = new ArrayAdapter<String>(this, R.layout.content_uom_list, R.id.text_username, uomArray);
+        ListView listViewUom = (ListView) findViewById(R.id.list_view_uom);
+
+        listViewUom.setAdapter(uomAdpter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
